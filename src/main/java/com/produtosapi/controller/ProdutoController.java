f97f0c9b81c1afee272bf53b0c34b6e628439127
samuel <samuka.jvm@gmail.com>
2026-08-07 14:43:15 -0400

    09.Deletando produtos

--- src/main/java/com/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/produtosapi/controller/ProdutoController.java
@@ -1,20 +1,17 @@
 package com.produtosapi.controller;
 
-import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.produtosapi.model.Produto;
 import com.produtosapi.repository.ProdutoRepository;
-
-import jakarta.websocket.server.PathParam;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("produtos")
@@ -39,6 +36,11 @@ public class ProdutoController {
     @GetMapping("/{id}")
     public Produto obterPorID(@PathVariable String id) {
         return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
     }
 
 }
