11557656490b3d9d85e52ad9565f9a832d5c8402
samuel <samuka.jvm@gmail.com>
2026-08-07 14:28:09 -0400

    08.Obtendo os dados do produto

--- src/main/java/com/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/produtosapi/controller/ProdutoController.java
@@ -1,5 +1,6 @@
 package com.produtosapi.controller;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.produtosapi.model.Produto;
 import com.produtosapi.repository.ProdutoRepository;
+
+import jakarta.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("produtos")
@@ -27,6 +34,11 @@ public class ProdutoController {
         produto.setId(id);
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorID(@PathVariable String id) {
+        return produtoRepository.findById(id).orElse(null);
     }
 
 }
