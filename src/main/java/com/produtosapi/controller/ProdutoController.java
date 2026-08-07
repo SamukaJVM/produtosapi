2d7277c00d7eaa5707ccc3da369f672a8fe9e17a
samuel <samuka.jvm@gmail.com>
2026-08-07 10:22:00 -0400

    07.Persistindo produtos no banco de dados

--- src/main/java/com/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/produtosapi/controller/ProdutoController.java
@@ -1,18 +1,31 @@
 package com.produtosapi.controller;
 
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.produtosapi.model.Produto;
+import com.produtosapi.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
         return produto;
     }
 
---
+++ src/main/java/com/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.produtosapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.produtosapi.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+}
